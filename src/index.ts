abeb912faa3839617e3cac936ba1fd84660ba118
Benoit Lemoine <benoit.lemoine@busbud.com>
2023-02-25 18:06:01 -0500

    feat: try supporting cli

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
+/*
 import { Project } from "ts-morph";
 import { removeAny } from "./remove-any/remove-any";
 
 const project = new Project();
 
-const dir = __dirname + "/__tests__/**/*{.d.ts,.ts}";
+const dir = __dirname + "/__tests__/!**!/!*{.d.ts,.ts}";
 console.log("Directory", dir);
 project.addSourceFilesAtPaths(dir);
 project.resolveSourceFileDependencies();
@@ -13,3 +14,37 @@ project.getSourceFiles().forEach((sourceFile) => {
   console.log("#### RESULT ####");
   console.log(sourceFile.print());
 });
+*/
+
+import { parseArgs } from "node:util";
+import { readdir } from "node:fs/promises";
+
+async function main(args: string[]) {
+  const options = {
+    directory: {
+      type: "string",
+      short: "d",
+      default: ".",
+    },
+  } as const;
+
+  const { values } = parseArgs({ args, options, strict: true });
+  const { directory } = values;
+
+  if (!directory) {
+    throw new Error(`Directory parameter is mandatory`);
+  }
+
+  const files = await readdir(directory);
+  const tsconfigFile = "tsconfig.json";
+  if (!files.includes(tsconfigFile)) {
+    throw new Error(`The directory must contain a tsconfig.json file`);
+  }
+  
+  return `${directory}/${tsconfigFile}`;
+}
+
+const args = process.argv.slice(2);
+main(args)
+  .then((s) => console.log("success", s))
+  .catch((e) => console.error("error", e));
