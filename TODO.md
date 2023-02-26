b74cd98c1572f02252de0507e1d28a09770176b2
Benoit Lemoine <benoit.lemoine@busbud.com>
2023-02-26 10:12:45 -0500

    refactor: extract getParameterComputedType

---
+++ TODO.md
@@ -0,0 +1,4 @@
+* Use diagnostic to check if the change break the project
+* remove any for `const` declaration
+* Optimize performances
+* 
--- src/remove-any/remove-any.ts
+++ src/remove-any/remove-any.ts
@@ -1,66 +1,71 @@
-import { CallExpression, FunctionDeclaration, SourceFile } from "ts-morph";
+import { CallExpression, FunctionDeclaration, ParameterDeclaration, SourceFile } from "ts-morph";
+
+function getParameterComputedType(
+  parametersFn: ParameterDeclaration,
+  sourceFn: FunctionDeclaration,
+  parametersIdx: number
+): string | null {
+  const isAny = parametersFn.getType().isAny();
+  const declaredType = parametersFn.getTypeNode();
+  const isImplicitAny = isAny && !declaredType;
+
+  if (!isImplicitAny) {
+    return null;
+  }
+
+  const callsiteTypes = sourceFn
+    .findReferencesAsNodes()
+    .map((ref) => {
+      const parent = ref.getParent();
+      if (parent instanceof CallExpression) {
+        const argument = parent.getArguments()[parametersIdx];
+        return argument?.getType();
+      } else {
+        return null;
+      }
+    })
+    .filter(isNotNil)
+    .filter((t) => !t.isAny() && !t.getText().includes("any[]"))
+    .filter((t) => !t.getText().startsWith("import(") && !t.getText().startsWith("typeof"));
+
+  if (callsiteTypes.length === 0) {
+    return null;
+  }
+  if (callsiteTypes.every((s) => s.isBooleanLiteral() || s.isBoolean())) {
+    return "boolean";
+  }
+  if (callsiteTypes.length === 1) {
+    return callsiteTypes[0].getText();
+  }
+  if (callsiteTypes.length <= 4) {
+    if (callsiteTypes.every((t) => t.isNumber() || t.isNumberLiteral()) && callsiteTypes.some((t) => t.isNumber())) {
+      return "number";
+    }
+    if (callsiteTypes.every((t) => t.isString() || t.isStringLiteral()) && callsiteTypes.some((t) => t.isString())) {
+      return "string";
+    }
+
+    const newTypes = [...new Set(callsiteTypes.map((t) => t.getText()))];
+    return newTypes.join(" | ");
+  }
+
+  if (callsiteTypes.every((t) => t.isNumber() || t.isNumberLiteral())) {
+    return "number";
+  } else if (callsiteTypes.every((t) => t.isString() || t.isStringLiteral())) {
+    return "string";
+  }
+  return null;
+}
 
 function removeAnyInFunction(sourceFn: FunctionDeclaration): number {
   return sourceFn
     .getParameters()
     .map((parametersFn, parametersIdx) => {
-      const isAny = parametersFn.getType().isAny();
-      const declaredType = parametersFn.getTypeNode();
-      const isImplicitAny = isAny && !declaredType;
-
-      if (isImplicitAny) {
-        const callsiteTypes = sourceFn
-          .findReferencesAsNodes()
-          .map((ref) => {
-            const parent = ref.getParent();
-            if (parent instanceof CallExpression) {
-              const argument = parent.getArguments()[parametersIdx];
-              return argument?.getType();
-            } else {
-              return null;
-            }
-          })
-          .filter(isNotNil)
-          .filter((t) => !t.isAny() && !t.getText().includes("any[]"))
-          .filter((t) => !t.getText().startsWith("import(") && !t.getText().startsWith("typeof"));
-
-        if (callsiteTypes.length === 0) {
-          return null;
-        }
-        let newType: string | null = null;
-        if (callsiteTypes.every((s) => s.isBooleanLiteral() || s.isBoolean())) {
-          newType = "boolean";
-        } else if (callsiteTypes.length === 1) {
-          newType = callsiteTypes[0].getText();
-        } else if (callsiteTypes.length <= 4) {
-          if (
-            callsiteTypes.every((t) => t.isNumber() || t.isNumberLiteral()) &&
-            callsiteTypes.some((t) => t.isNumber())
-          ) {
-            newType = "number";
-          } else if (
-            callsiteTypes.every((t) => t.isString() || t.isStringLiteral()) &&
-            callsiteTypes.some((t) => t.isString())
-          ) {
-            newType = "string";
-          } else {
-            const newTypes = [...new Set(callsiteTypes.map((t) => t.getText()))];
-            newType = newTypes.join(" | ");
-          }
-        } else {
-          if (callsiteTypes.every((t) => t.isNumber() || t.isNumberLiteral())) {
-            newType = "number";
-          } else if (callsiteTypes.every((t) => t.isString() || t.isStringLiteral())) {
-            newType = "string";
-          }
-        }
-
-        if (newType) {
-          parametersFn.setType(newType);
-        }
-        return newType;
+      const newType = getParameterComputedType(parametersFn, sourceFn, parametersIdx);
+      if (newType) {
+        parametersFn.setType(newType);
       }
-      return null;
+      return newType;
     })
     .filter(isNotNil).length;
 }
