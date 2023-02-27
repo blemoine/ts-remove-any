170b5338a2d066815af679c4d3b307f7f1d63873
Benoit Lemoine <benoit.lemoine@busbud.com>
2023-02-27 09:44:19 -0500

    refactor: extract common revertable operation

--- src/remove-any/remove-any.ts
+++ src/remove-any/remove-any.ts
@@ -2,36 +2,34 @@ import { SourceFile } from "ts-morph";
 import { removeAnyInFunction } from "./remove-any-in-function-parameters";
 import { sum } from "../utils/array.utils";
 import { removeAnyInLetDeclaration } from "./remove-any-in-let-declaration";
+import { RevertableOperation } from "./revert-operation";
 
 export function removeAny(sourceFile: SourceFile): number {
   if (sourceFile.getBaseName().endsWith("js")) {
     return 0;
   }
-  const resultsInFunctions = sourceFile.getFunctions().map((sourceFn) => {
-    const preChangeDiagnostic = sourceFile.getPreEmitDiagnostics();
-    const result = removeAnyInFunction(sourceFn);
-    const postChangeDiagnostic = sourceFile.getPreEmitDiagnostics();
-    if (postChangeDiagnostic.length > preChangeDiagnostic.length) {
-      console.warn(`Reverting ${sourceFile.getBaseName()}`);
-      result.revert();
-      return 0;
-    }
+  const resultsInFunctions = sourceFile
+    .getFunctions()
+    .map((sourceFn) => revertableOperation(sourceFile, () => removeAnyInFunction(sourceFn)));
 
-    return result.countChangesDone;
-  });
-
-  const resultsInLets = sourceFile.getVariableDeclarations().map((variableDeclaration) => {
-    const preChangeDiagnostic = sourceFile.getPreEmitDiagnostics();
-    const result = removeAnyInLetDeclaration(variableDeclaration);
-    const postChangeDiagnostic = sourceFile.getPreEmitDiagnostics();
-    if (postChangeDiagnostic.length > preChangeDiagnostic.length) {
-      console.warn(`Reverting ${sourceFile.getBaseName()}`);
-      result.revert();
-      return 0;
-    }
-
-    return result.countChangesDone;
-  });
+  const resultsInLets = sourceFile
+    .getVariableDeclarations()
+    .map((variableDeclaration) =>
+      revertableOperation(sourceFile, () => removeAnyInLetDeclaration(variableDeclaration))
+    );
 
   return sum(resultsInFunctions) + sum(resultsInLets);
+}
+
+function revertableOperation(sourceFile: SourceFile, revertableFn: () => RevertableOperation) {
+  const preChangeDiagnostic = sourceFile.getPreEmitDiagnostics();
+  const result = revertableFn();
+  const postChangeDiagnostic = sourceFile.getPreEmitDiagnostics();
+  if (postChangeDiagnostic.length > preChangeDiagnostic.length) {
+    console.warn(`Reverting ${sourceFile.getBaseName()}`);
+    result.revert();
+    return 0;
+  }
+
+  return result.countChangesDone;
 }
